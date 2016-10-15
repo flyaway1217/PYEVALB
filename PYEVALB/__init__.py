f5906bd8cdb9f3a9e56eee8e06555abd6cc3293d
flyaway1217 <flyaway1217@gmail.com>
2016-10-15 16:07:12 -0600

    add versions info

---
+++ PYEVALB/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+# Author: Flyaway - flyaway1217@gmail.com
+# Blog: zhouyichu.com
+#
+# Python release: 3.4.5
+#
+# Date: 2016-10-15 16:02:23
+# Last modified: 2016-10-15 16:02:35
+
+"""
+
+"""
+__version__ = '0.1.0'
